6f4e562c287b947eebdb87d648fd214bd8e28c93
Slaers-aka <60404225+Slaers-aka@users.noreply.github.com>
2020-03-23 16:20:17 +0200

    Back Resize

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(".nav-icon").click(function(event) {
     $('.nav-icon, .nav').toggleClass('active');
     $('body').toggleClass('lock');
 })
+
 $('.footer-h4').click(function(event){
     var width = $(window).width();
     if (width <= 750) {
@@ -11,4 +12,16 @@ $('.footer-h4').click(function(event){
         }
         $(this).toggleClass('active-footer').next().slideToggle(300);
     } 
+})
+
+var windowsize = $(window).width();
+$(window).resize(function() {
+    windowsize = $(window).width();
+    if (windowsize > 750) {
+        $('.save-footer-item-container').css({'display' : 'block'});
+    }
+    else {
+        $('.save-footer-item-container').css({'display' : 'none'});
+    }
+    $('.footer-h4').removeClass('active-footer');
 })
